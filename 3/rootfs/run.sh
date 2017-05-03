6cee06e3e964bb65b0ca75a7a488eab79a157a47
Tihomir Ivanov <office@ivanovnewmedia.com>
2017-05-02 23:45:59 -0400

    added vm.overcommit_memory

--- 3/rootfs/run.sh
+++ 3/rootfs/run.sh
@@ -10,5 +10,9 @@ ARGS="/opt/bitnami/redis/conf/redis.conf --daemonize no"
 # log output to stdout
 sed -i 's/^logfile /# logfile /g' /opt/bitnami/redis/conf/redis.conf
 
+echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
+sysctl vm.overcommit_memory = 1
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
 info "Starting ${DAEMON}..."
 exec gosu ${USER} ${EXEC} ${ARGS}
